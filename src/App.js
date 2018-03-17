d79c60e0439d20ab392de392f653d65796c5d4e0
Ben Lewis <bdlewis30@gmail.com>
2018-03-17 03:14:09 -0600

    Added reset.css and some basic styling.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import './reset.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <header>
+          <h1>This is the front page</h1>
+        </header>
       </div>
     );
   }
